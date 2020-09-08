5a88cc1651a6daac8d199ecb5be72c3da1fb7c0c
aljimenez5 <arvida.jimenez@gmail.com>
2020-09-07 23:42:01 -0400

    create flowers controller

--- flower-breeding-tracker-backend/app/controllers/flowers_controller.rb
+++ flower-breeding-tracker-backend/app/controllers/flowers_controller.rb
@@ -1,4 +1,41 @@
 class FlowersController < ApplicationController
     
+    def index 
+        flowers = Flower.all 
+        render json: FlowerSerializer.new(flowers)
+    end
+
+    def show 
+        flower = Flower.find_by(id: params[:id])
+        render json: FlowerSerializer.new(flower)
+    end
+
+    def new
+    end
+
+    def create
+        flower = Flower.find_by(id: params["flower_id"])
+        if !flower 
+            flower = Flower.create(flower_params)
+
+            render json: FlowerSerializer.new(flower)
+        else
+            render json: FlowerSerializer.new(flower) {status: 'error', message: "Land already exists"}
+        end
+        
+    end
+
+    def destroy
+        flower = Flower.find_by(id: params["id"])
+        flower.delete
+        render json: FlowerSerializer.new(flower)
+    end
+
+    private
+
+    def flower_params
+        params.require(:flower).permit(:name, :color, :origin, :image_url)
+
+    end
     
 end
