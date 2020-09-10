aa2dbf338eadee7c77951d9eb8bdeae3ad0f783b
aljimenez5 <arvida.jimenez@gmail.com>
2020-09-10 03:13:40 -0400

    adds foreign keys again

--- flower-breeding-tracker-backend/db/schema.rb
+++ flower-breeding-tracker-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_013557) do
+ActiveRecord::Schema.define(version: 2020_09_10_022125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_09_10_013557) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "square_id"
+    t.integer "parent_couple_id"
   end
 
   create_table "lands", force: :cascade do |t|
