d9f95f2f4e33742ef4c00c5b94fcb9b5635a8ae2
aljimenez5 <arvida.jimenez@gmail.com>
2020-09-10 03:13:09 -0400

    updates associations

--- flower-breeding-tracker-backend/app/serializers/flower_serializer.rb
+++ flower-breeding-tracker-backend/app/serializers/flower_serializer.rb
@@ -1,5 +1,6 @@
 class FlowerSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :color, :origin, :image_url
-  belongs_to :parent_couple
+  belongs_to :parent_couple, optional: true
+  belongs_to :square, optional: true
 end
--- flower-breeding-tracker-backend/app/serializers/square_serializer.rb
+++ flower-breeding-tracker-backend/app/serializers/square_serializer.rb
@@ -1,6 +1,6 @@
 class SquareSerializer
   include FastJsonapi::ObjectSerializer
   attributes :column_num, :row_num
-  has_one :flower
+  has_one :flower, optional: true
   belongs_to :land
 end
