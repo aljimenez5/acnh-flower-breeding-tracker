7e4ce98cb267fa0d3040fa5be667308575257681
aljimenez5 <arvida.jimenez@gmail.com>
2020-09-14 03:02:41 -0400

    add land plot and fix more UI

--- flower-breeding-tracker-frontend/src/index.js
+++ flower-breeding-tracker-frontend/src/index.js
@@ -6,8 +6,10 @@ const SQUARES_URL = `${BACKEND_URL}/squares`
 
 
 let getFlowersByType = function(event, flowerType, status="allCards") {
-    console.log(event)
-    console.log(event.target)
+    
+    let flowerDiv = document.getElementById("all-colors-selection")
+    flowerDiv.innerHTML = ""
+
     const requestConfigs = {
         method: 'GET',
         headers: {
@@ -16,7 +18,7 @@ let getFlowersByType = function(event, flowerType, status="allCards") {
         }
     }
 
-    fetch(FLOWERS_URL, requestConfigs).then(response => response.json()).then(parsedResponse => parsedResponse.data.filter(obj => obj.attributes.name === flowerType && obj.attributes.color !== "White")).then(filteredResponse => filteredResponse.forEach(obj => createFlowerCard(obj.attributes, status)))
+    fetch(FLOWERS_URL, requestConfigs).then(response => response.json()).then(parsedResponse => parsedResponse.data.filter(obj => obj.attributes.name === flowerType)).then(filteredResponse => filteredResponse.forEach(obj => createFlowerCard(obj, status)))
 
     event.target.addEventListener("click", function (e) { removeFlowerDiv(e, flowerType) }, { once: true })
 }
@@ -30,8 +32,48 @@ let getWhiteFlowers = function(status="onlyWhiteCards") {
         }
     }
 
-    fetch(FLOWERS_URL, requestConfigs).then(response => response.json()).then(parsedResponse => parsedResponse.data.filter(obj => obj.attributes.color === "White")).then(filteredResponse => filteredResponse.forEach(obj => createFlowerCard(obj.attributes, status)))
+    fetch(FLOWERS_URL, requestConfigs).then(response => response.json()).then(parsedResponse => parsedResponse.data.filter(obj => obj.attributes.color === "White")).then(filteredResponse => filteredResponse.forEach(obj => createFlowerCard(obj, status)))
 
+}
+
+let getFlower = function(flowerObj, squareObj, status="squareFlower") {
+    const requestConfigs = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+
+    if (flowerObj)
+        {fetch(`${FLOWERS_URL}/${flowerObj.id}`, requestConfigs).then(response => response.json()).then(parsedResponse => createFlowerCard(parsedResponse.data, status, squareObj))}
+
+    else {
+        addEmptyPlotToSquare(squareObj)
+    }
+}
+
+let getLandPlot = function(landObjID = 1) {
+    const requestConfigs = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+
+    fetch(`${LANDS_URL}/${landObjID}`, requestConfigs).then(response => response.json()).then(parsedResponse => displayLandPlot(parsedResponse.data))
+}
+
+let getSquare = function(squareObj) {
+    const requestConfigs = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+    fetch(`${SQUARES_URL}/${squareObj.id}`, requestConfigs).then(response => response.json()).then(parsedResponse => getFlower(parsedResponse.data.relationships.flower.data, parsedResponse.data))
 }
 
 let createDiv = function(name, className=name){
@@ -41,26 +83,42 @@ let createDiv = function(name, className=name){
     return newDiv
 }
 
-let createFlowerCard = function(obj, status) {
-    let newFlowerCard
-    if (status === "onlyWhiteCards") {
-        newFlowerCard = createDiv(`main-${obj.name}-card`, "main-flower-card")
-        let text = (`${obj.name}`)
-        newFlowerCard.textContent = text
-    }
-
-    else {
-        newFlowerCard = createDiv(`${obj.name} ${obj.color}`, "flower-card")
-    }
-    
+let createImageElement = function(obj){
     let image = document.createElement("img")
     image.src = `./assets/images/${obj.image_url}`
     image.height = 100
     image.width = 100
-    
-    newFlowerCard.appendChild(image)
+    return image
+}
 
-    addFlowerToHeader(obj, newFlowerCard, status)
+let createFlowerCard = function(obj, status, otherObj= null) {
+    let newFlowerCard
+    let newObj = obj.attributes
+    
+    if (status === "onlyWhiteCards") {
+        newFlowerCard = createDiv(`main-${newObj.name}-card`, "main-flower-card")
+        let text = (`${newObj.name}`)
+        newFlowerCard.textContent = text
+        let image = createImageElement(newObj)
+        newFlowerCard.appendChild(image)
+        addFlowerToHeader(newObj, newFlowerCard, status)
+    }
+
+    else if (status === "allCards"){
+        
+        newFlowerCard = createDiv(`${newObj.name} ${newObj.color}`, "selection-flower-card")
+        let image = createImageElement(newObj)
+        newFlowerCard.appendChild(image)
+        addFlowersAboveFlowerDiv(newObj, newFlowerCard, status)
+    }
+
+    else {
+        
+        newFlowerCard = createDiv(`${newObj.name} ${newObj.color}`, "flower-card")
+        let image = createImageElement(newObj)
+        newFlowerCard.appendChild(image)
+        addFlowerToSquare(obj, newFlowerCard, status, otherObj)
+    }
     
 }
 
@@ -68,16 +126,18 @@ let createFlowerCard = function(obj, status) {
 let addFlowerToHeader = function (obj, card, status) {
     let flowerSelectionHeader = document.getElementById("flower-selection")
     if (status === "onlyWhiteCards") {
-        let expand = createDiv("button-container")
-        let btn = document.createElement('button')
-        btn.className = "open-close-button"
+        // let expand = createDiv("button-container")
+        // let btn = document.createElement('button')
+        // btn.className = "open-close-button"
 
 
-        expand.addEventListener("click", function(event){getFlowersByType(event, obj.name)}, {once: true})
+        card.addEventListener("click", function(event){getFlowersByType(event, obj.name)}, {once: true})
 
-        flowerSelectionHeader.appendChild(card).appendChild(expand).appendChild(btn)
+        flowerSelectionHeader.appendChild(card)
+        // .appendChild(expand).appendChild(btn)
     }
     else {
+
         let mainFlowerDiv = document.getElementById(`main-${obj.name}-card`)
         let flowerSpecificDiv = document.getElementById(`${obj.name}`)
         if (flowerSpecificDiv === null) {
@@ -91,13 +151,20 @@ let addFlowerToHeader = function (obj, card, status) {
 
 }
 
+let addFlowersAboveFlowerDiv = function (newObj, newFlowerCard, status) {
+    let allColorsDiv = document.getElementById("all-colors-selection")
+    allColorsDiv.appendChild(newFlowerCard)
+}
+
 let removeFlowerDiv = function (event, flower) {
-    let flowerDiv = document.getElementById(flower)
-    flowerDiv.remove()
+    let flowerDiv = document.getElementById("all-colors-selection")
+    flowerDiv.innerHTML = ""
     event.target.addEventListener("click", function(e){
         getFlowersByType(e, flower)
     }, {once: true})
 }
+
+
 
 let createLandForm = function(){
     let selectColRowDiv = document.getElementById("column-row-select")
@@ -130,10 +197,65 @@ let createLandForm = function(){
 
 }
 
+let displayLandPlot = function(obj) {
+    
+    let landPlotBody = document.getElementById("land-plot-body")
+
+    let numRows = obj.attributes.number_of_rows
+    let numCols = obj.attributes.number_of_columns
+
+
+    let squaresInLand = obj.relationships.squares.data
+    console.log(squaresInLand)
+
+    for (let i = 0; i < numRows; i++) {
+        let tableRow = document.createElement("tr")
+        tableRow.id = i + 1
+        landPlotBody.appendChild(tableRow)
+
+        for (let ii = 0; ii < numCols; ii++) {
+            let tableCol = document.createElement("td")
+            tableCol.id = ii + 1
+            tableCol.className = "land-plot-squares"
+            tableRow.appendChild(tableCol)
+
+        }
+        
+    }
+
+
+    obj.relationships.squares.data.forEach(squareObj => getSquare(squareObj))
+
+
+}
+
+
+let addFlowerToSquare = function (obj, newFlowerCard, status, squareObj) {
+    // console.log(obj)
+    console.log(newFlowerCard)
+
+    // console.log(squareObj.attributes)
+
+    let flowerRow = squareObj.attributes.row_num
+    let flowerCol = squareObj.attributes.column_num
+
+    let tableRow = document.getElementsByTagName("tr")
+    let trElem = tableRow[flowerRow - 1]
+    let tdElem = trElem.getElementsByTagName("td")
+    tdElem[flowerCol - 1].appendChild(newFlowerCard)
+
+
+}
+
+let addEmptyPlotToSquare = function(squareObj) {
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
     createLandForm()
     getWhiteFlowers()
+    getLandPlot()
 
 
 })
